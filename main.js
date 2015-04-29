aebe1792b9c289cebd75cec3ef825aa9998feefa
dizehacioglu <dh1272@nyu.edu>
2015-04-29 15:12:36 -0600

    Added form submission functionality

--- main.js
+++ main.js
@@ -10,47 +10,100 @@ $(document).on('ready', function() {
 		this.downvotes = 0;
 		// this.category = category;
 		// this.picture = picture;
-
-		$('body').append(this.render());
 	};
 
 	Event.prototype.render = function(){
-		this.el = $('#event-tpl')
-		.clone()
-		.attr('id', null)
-		.addClass('event');
-
+		
+		if(this.el === undefined){
+			this.el = $('#event-tpl')
+			.clone()
+			.attr('id', null)
+			.addClass('event');
+		}
+		
 		this.el.find('.event-title').text(this.title);
 		this.el.find('.event-date').text(this.date);
 		this.el.find('.event-description').text(this.description);
 		this.el.find('.event-location').text(this.location);
 
+
 		return this.el;
 	};
 
-	var EventList = function(){
-
+	var EventLibrary = function(){
+		this.events = [];
+		this.render();
 	};
 
-	var User = function(name, description){
-		this.name = name;
-		this.description = description;
-		this.postedEvents = [];
-		this.interestedEvents = [];
+	EventLibrary.prototype.addEvent = function(event){
+		this.events.push(event);
+		this.render();
 	};
 
-	$('.add-event-form').on('click', function(){
-		console.log("clicked on add event");
-	});
+	EventLibrary.prototype.render = function(){
+		if(this.el === undefined){
+			this.el = $('#event-library-tpl')
+			.clone()
+			.attr('id', null)
+			.addClass('event-library');
 
+			var originalLibrary = this;
+
+			$('.add-event-form').on('click', function(){
+				var eventTitle = $('body').find('[name=event-title]').val();
+				var eventDate = $('body').find('[name=event-date]').val();
+				var eventDescription = $('body').find('[name=event-description]').val();
+				var eventLocation = $('body').find('[name=event-location]').val();
+
+				var newEvent = new Event(eventTitle, eventDate, eventDescription, eventLocation);
+				originalLibrary.addEvent(newEvent);
+			});
+		}
+
+		var eventElements = this.events.map(function(e){
+			return e.render();
+		});
+
+		$('body').append(eventElements);
+	};
+
+	EventLibrary.prototype.onFormSubmit = function(e){
+		console.log(e);
+		console.log(this);
+	};
+
+	// var User = function(name, description){
+	// 	this.name = name;
+	// 	this.description = description;
+	// 	this.postedEvents = [];
+	// 	this.interestedEvents = [];
+	// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	var library = new EventLibrary();
+
+
+	// Fake Events
 	var e = new Event('Drake Concert', 'October 26th', 'Come listen to Champagne Papi cry about me', 'Pepsi Center');
+	var e2 = new Event('Picnic', 'Right now', 'It\'s nice outside and I want to eat', 'Chautauqua');
 
-	// $('body').append(e.render());
-
-	var e2 = new Event('Picnic', 'Right now', 'It\'s nice outside and I want to eat', 'Chataqua');
-	// $('body').append(e2.render());
-
-
+	library.addEvent(e);
+	library.addEvent(e2);
 
 
 
